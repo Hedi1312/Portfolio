3f634b921e4b6dbb16e5fce902544214260c6e62
Hedi1312 <hediokba18@gmail.com>
2026-03-09 03:19:59 +0100

    fix : fix prettier and build

--- src/app/sections/APropos.tsx
+++ src/app/sections/APropos.tsx
@@ -3,9 +3,9 @@ export default function APropos() {
     <section id="a-propos" className="px-6 py-20 bg-neutral-900 text-center text-white">
       <h3 className="text-3xl font-bold mb-6">À propos</h3>
       <p className="max-w-3xl mx-auto text-neutral-300 leading-relaxed">
-        Je suis un développeur front-end, passionné par la création d’interfaces
-        élégantes et performantes. J’aime transformer des idées en expériences concrètes avec un
-        soin particulier pour le design et la performance.
+        Je suis un développeur front-end, passionné par la création d’interfaces élégantes et
+        performantes. J’aime transformer des idées en expériences concrètes avec un soin particulier
+        pour le design et la performance.
       </p>
     </section>
   );
